e9c1361b1511ef8609163af47771c28295cf260f
Jasmin Gelbmann <jasmin.gelbmann@gmail.com>
2021-02-15 11:01:53 +0100

    Add exercise

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner grades = new Scanner(System.in);
+        int myGrades = grades.nextInt();
+
+            String result = switch (myGrades) {
+            case 1 : yield "Very good";
+            case 2 : yield "Good";
+            case 3 : yield "Satisfactory";
+            case 4 : yield "Sufficient";
+            case 5 : yield "Not sufficient";
+                default: yield "This isn't a grade";
+        };
+    }
+}
