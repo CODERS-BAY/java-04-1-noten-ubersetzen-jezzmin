4b0bc23c90f7a03d294630e35c2bbf641e0da153
Jasmin Gelbmann <jasmin.gelbmann@gmail.com>
2021-02-15 11:08:55 +0100

    Processed

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,19 @@ public class Main {
         Scanner grades = new Scanner(System.in);
         int myGrades = grades.nextInt();
 
-            String result = switch (myGrades) {
-            case 1 : yield "Very good";
-            case 2 : yield "Good";
-            case 3 : yield "Satisfactory";
-            case 4 : yield "Sufficient";
-            case 5 : yield "Not sufficient";
-                default: yield "This isn't a grade";
+        String result = switch (myGrades) {
+            case 1:
+                yield "Very good";
+            case 2:
+                yield "Good";
+            case 3:
+                yield "Satisfactory";
+            case 4:
+                yield "Sufficient";
+            case 5:
+                yield "Not sufficient";
+            default:
+                yield "This isn't a grade";
         };
     }
 }
